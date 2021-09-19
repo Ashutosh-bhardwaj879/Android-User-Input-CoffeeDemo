8c1e89540c555cadb1c3cd66b3ba5a851830f802
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-09-19 15:29:09 +0530

    CLASS 18

--- app/src/main/java/com/meicode/justjava/MainActivity.java
+++ app/src/main/java/com/meicode/justjava/MainActivity.java
@@ -3,12 +3,24 @@ package com.meicode.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+//CLASS 18 STARTED
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //resource.layout.activitymain
+    }
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    private void display(int numbers){
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(" "+numbers);
     }
 }

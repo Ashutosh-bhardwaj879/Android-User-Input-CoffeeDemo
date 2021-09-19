ba3d026376c6dcc040b5975eee268f8c5fe65530
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-09-19 16:11:54 +0530

    CLASS 21

--- app/src/main/java/com/meicode/justjava/MainActivity.java
+++ app/src/main/java/com/meicode/justjava/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
 //CLASS 18 STARTED
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +20,20 @@ public class MainActivity extends AppCompatActivity {
         //resource.layout.activitymain
     }
 
-//    Button          android:onClick="submitOrder" this is defined in main.xml file so used here
-    public void submitOrder(View view){
+    //    Button          android:onClick="submitOrder" this is defined in main.xml file so used here
+    public void submitOrder(View view) {
         display(1);
     }
 
-    private void display(int numbers){
+    private void display(int numbers) {
         //finding the quantity initial value
-        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         //setting new value to the quantity text
-        quantityTextView.setText(" "+numbers);
+        quantityTextView.setText(" " + numbers);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }

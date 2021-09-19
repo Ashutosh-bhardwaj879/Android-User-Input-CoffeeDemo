12932058c44ea6104d66ec1ff7ee04afb6118c31
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-09-19 15:59:48 +0530

    CLASS 20

--- app/src/main/java/com/meicode/justjava/MainActivity.java
+++ app/src/main/java/com/meicode/justjava/MainActivity.java
@@ -15,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //resource.layout.activitymain
     }
+
+//    Button          android:onClick="submitOrder" this is defined in main.xml file so used here
     public void submitOrder(View view){
         display(1);
     }
 
     private void display(int numbers){
+        //finding the quantity initial value
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        //setting new value to the quantity text
         quantityTextView.setText(" "+numbers);
     }
 }

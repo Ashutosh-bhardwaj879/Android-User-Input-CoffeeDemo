85fdbd2ee3fcb83c6c4d99e7538a3eb376d0dce3
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-09-29 22:07:39 +0530

    learned how to add text

--- app/src/main/java/com/meicode/justjava/MainActivity.java
+++ app/src/main/java/com/meicode/justjava/MainActivity.java
@@ -3,12 +3,11 @@ package com.meicode.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
 
 
 //import java.text.NumberFormat;
@@ -16,6 +15,7 @@ import android.widget.TextView;
 //CLASS 18 STARTED
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,36 +25,47 @@ public class MainActivity extends AppCompatActivity {
 
     //    Button          android:onClick="submitOrder" this is defined in main.xml file so used here
     public void submitOrder(View view) {
-    //working on check box
+        //edit text and find view by id
+        EditText nameField = findViewById(R.id.name_field);
+
+        //change edit text datatype to string
+        String name = nameField.getText().toString();
+
+        //working on check box
         CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
-    //checking is check box ticked or not
-        boolean hasWhippedCream  = whippedCreamCheckBox.isChecked();
+        //checking is check box ticked or not
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        //working on chocolate check box
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        //checking the chocolate check box
+        boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        int price=calculatePrice();
-        displayMessage(createOrderSummary(price,hasWhippedCream));
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(name,price, hasWhippedCream, hasChocolate));
     }
 
-    private int calculatePrice(){
-        return quantity*5;
+    private int calculatePrice() {
+        return quantity * 5;
     }
 
-    private String createOrderSummary(int price,boolean hasWhippedCream){
-        String priceMessage = "Name : Ashutosh Bhardwaj";
+    private String createOrderSummary(String name,int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String priceMessage = "Name : " + name;
         priceMessage += "\nAdded Whipped Cream? " + hasWhippedCream;
-        priceMessage += "\nQuantity "  + quantity;
+        priceMessage += "\nAdded Chocolate? " + hasChocolate;
+        priceMessage += "\nQuantity " + quantity;
         priceMessage += "\nTotal : $ " + price;
         priceMessage += "\nThank You";
         return priceMessage;
     }
 
 
-    private void displayMessage(String message){
+    private void displayMessage(String message) {
         TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
 
     }
 
-//when we click  +  button
+    //when we click  +  button
     public void increment(View view) {
         quantity = quantity + 1;
         if (quantity >= 0) {
@@ -62,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//when we click  -  button
+    //when we click  -  button
     public void decrement(View view) {
         quantity = quantity - 1;
         if (quantity >= 0) {
